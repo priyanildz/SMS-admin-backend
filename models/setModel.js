d5199602ace7f8cc7f50c1599e950a55a8c37c4a
Priyail <nidhipandey8108@gmail.com>
2025-11-29 18:01:33 +0530

    update

--- models/setModel.js
+++ models/setModel.js
@@ -4,7 +4,7 @@ const setSchema = new mongoose.Schema({
   standard: { type: String, required: true },
   subject: { type: String, required: true },
   name: { type: String, required: true }, // e.g. "Set 1"
-  url: { type: String, required: true },  // file link / PDF URL
+  pdfpath: { type: String, required: true },  // file link / PDF URL
 });
 
 module.exports = mongoose.model("paper-set", setSchema);
