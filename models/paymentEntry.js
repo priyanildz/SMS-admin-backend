720161e18c4282e67c6475b174d17a1c52060196
Priyail <nidhipandey8108@gmail.com>
2025-12-02 18:10:52 +0530

    update

--- models/paymentEntry.js
+++ models/paymentEntry.js
@@ -1,42 +1,18 @@
-// const mongoose = require("mongoose");
-
-// const paymentEntrySchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   std: { type: String, required: true },
-//   div: { type: String, required: true },
-//   totalFees: { type: Number },
-//   status: { type: String, enum: ["Paid", "Unpaid"], required: true },
-//   installments: [
-//     {
-//       date: { type: Date, required: true },
-//       amount: { type: Number, required: true },
-//       mode: { type: String },
-//     },
-//   ],
-// });
-
-// module.exports = mongoose.model("PaymentEntry", paymentEntrySchema);
-
-
-
-
-
 const mongoose = require("mongoose");
 
 const paymentEntrySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  std: { type: String, required: true },
-  div: { type: String, required: true },
-  studentid: { type: String, required: true }, // NEW FIELD
-  totalFees: { type: Number },
-  status: { type: String, enum: ["Paid", "Unpaid"], required: true },
-  installments: [
-    {
-      date: { type: Date, required: true },
-      amount: { type: Number, required: true },
-      mode: { type: String },
-    },
-  ],
+  name: { type: String, required: true },
+  std: { type: String, required: true },
+  div: { type: String, required: true },
+  totalFees: { type: Number },
+  status: { type: String, enum: ["Paid", "Unpaid"], required: true },
+  installments: [
+    {
+      date: { type: Date, required: true },
+      amount: { type: Number, required: true },
+      mode: { type: String },
+    },
+  ],
 });
 
 module.exports = mongoose.model("PaymentEntry", paymentEntrySchema);
