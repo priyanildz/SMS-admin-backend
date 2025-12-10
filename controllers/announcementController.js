a4d6fcb935e23624fa780582d60ef650323078de
Priyail <nidhipandey8108@gmail.com>
2025-12-10 22:00:03 +0530

    update

--- controllers/announcementController.js
+++ controllers/announcementController.js
@@ -1,36 +1,80 @@
+// const announcement = require("../models/announcementModel");
+// // add announcement
+// exports.addAnnouncement = async (req, res) => {
+//   try {
+//     const response = new announcement(req.body);
+//     await response.save();
+//     return res.status(200).json({ message: "announcement added successfully" });
+//   } catch (error) {
+//     return res.status(500).json({ error: error.message });
+//   }
+// };
+
+// // get list of all announcement
+// exports.getAnnouncement = async (req, res) => {
+//   try {
+//     const response = await announcement.find();
+//     return res.status(200).json(response);
+//   } catch (error) {
+//     return res.status(500).json({ error: error.message });
+//   }
+// };
+
+// // modify draft mail to sent
+// exports.updateAnnouncement = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const response = await announcement.findByIdAndUpdate(
+//       id,
+//       { status: "sent" },
+//       { new: true }
+//     );
+//     return res.status(200).json({message:'updated successfully'})
+//   } catch (error) {
+//     return res.status(500).json({ error: error.message });
+//   }
+// };
+
+
+
+
+
+
 const announcement = require("../models/announcementModel");
 // add announcement
 exports.addAnnouncement = async (req, res) => {
-  try {
-    const response = new announcement(req.body);
-    await response.save();
-    return res.status(200).json({ message: "announcement added successfully" });
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
+  try {
+    const response = new announcement(req.body);
+    await response.save();
+    // Return the created object for flexibility
+    return res.status(200).json({ message: "announcement added successfully", data: response });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
 
 // get list of all announcement
 exports.getAnnouncement = async (req, res) => {
-  try {
-    const response = await announcement.find();
-    return res.status(200).json(response);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
+  try {
+    const response = await announcement.find();
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
 
 // modify draft mail to sent
 exports.updateAnnouncement = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const response = await announcement.findByIdAndUpdate(
-      id,
-      { status: "sent" },
-      { new: true }
-    );
-    return res.status(200).json({message:'updated successfully'})
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
+  try {
+    const { id } = req.params;
+    const response = await announcement.findByIdAndUpdate(
+      id,
+      { status: "sent" }, // 🔥 CHANGE: Status is "sent"
+      { new: true }
+    );
+    // Return the updated object
+    return res.status(200).json({message:'updated successfully', data: response})
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
