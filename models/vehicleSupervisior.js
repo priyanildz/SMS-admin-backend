808764c40b51af812b636ad6ea3bbbd79efba2e6
Priyail <nidhipandey8108@gmail.com>
2025-12-18 15:34:03 +0530

    update

--- models/vehicleSupervisior.js
+++ models/vehicleSupervisior.js
@@ -105,12 +105,12 @@ const StaffSchema = new mongoose.Schema({
     },
     
     // 🌟 NEW: Statutory IDs and Licenses 🌟
-    // licenseNumber: {
-    //     type: String,
-    //     sparse: true,
-    //     // Default is null, not required. Explicitly remove unique: true if it was there before.
-    //     default: null, // Optional for Supervisors
-    // },
+    licenseNumber: {
+        type: String,
+        sparse: true,
+        // Default is null, not required. Explicitly remove unique: true if it was there before.
+        default: null, // Optional for Supervisors
+    },
     aadhaarNumber: {
         type: String,
         required: true,
