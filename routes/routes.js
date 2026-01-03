a7c8255da1118be2c680f6058ee757b0bbfbf4d4
Priyail <nidhipandey8108@gmail.com>
2026-01-03 15:15:15 +0530

    update

--- routes/routes.js
+++ routes/routes.js
@@ -90,6 +90,8 @@ router.get("/admin-attendance", adminController.getAdminAttendance);
 // Route for bulk registration
 router.post("/bulk-register", studentController.bulkCreateStudents);
 
+router.delete("/delete-junior-kg", studentController.deleteJuniorKGStudents);
+
 // adding student api
 router.post("/addstudent", studentController.createUser);
 
