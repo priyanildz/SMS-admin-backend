679c38ec625a4d7c0bbb1107af1937ca4e89b326
Priyail <nidhipandey8108@gmail.com>
2026-02-26 21:11:31 +0530

    update

--- models/eventsModel.js
+++ models/eventsModel.js
@@ -29,8 +29,37 @@
 
 
 
+// // models/eventsModel.js
+// const mongoose = require("mongoose");
+
+// const EventSchema = new mongoose.Schema({
+//   eventname: {
+//     type: String,
+//     required: true,
+//   },
+//   date: {
+//     type: Date,
+//     required: true,
+//   },
+//   staffid: {
+//     type: String, 
+//     ref: "staff", 
+//   },
+//   managedby: String, 
+//   standard: String,
+//   division: String,
+//   // FIX: Changed to ObjectId array referencing the student model for population
+//   participants: [{ 
+//     type: mongoose.Schema.Types.ObjectId, 
+//     ref: "student" 
+//   }]
+// }, { timestamps: true });
+
+// module.exports = mongoose.model("event", EventSchema);
+
+
 // models/eventsModel.js
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const EventSchema = new mongoose.Schema({
   eventname: {
@@ -48,11 +77,12 @@ const EventSchema = new mongoose.Schema({
   managedby: String, 
   standard: String,
   division: String,
-  // FIX: Changed to ObjectId array referencing the student model for population
+  // Good fix on the ObjectId array!
   participants: [{ 
     type: mongoose.Schema.Types.ObjectId, 
     ref: "student" 
   }]
 }, { timestamps: true });
 
-module.exports = mongoose.model("event", EventSchema);
+// Using default export for ES Modules
+export default mongoose.model("event", EventSchema);
